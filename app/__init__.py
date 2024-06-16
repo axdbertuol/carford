6600d454038a65a15e387d5d24d9d94962de9a2a
Alexandre Bertuol <axdbertuol@gmail.com>
2024-06-16 17:02:51 -0300

    feat: initate jwt

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_jwt_extended import JWTManager
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase
@@ -10,15 +11,18 @@ class Base(DeclarativeBase):
 
 db = SQLAlchemy(model_class=Base)
 migrate = Migrate()
+jwt = JWTManager()
 
 
 def create_app():
     app = Flask(__name__)
+
     from app.config import Config
 
-    app.config.from_object(Config())
+    app.config.from_object(Config)
 
     db.init_app(app)
     migrate.init_app(app, db)
+    jwt.init_app(app)
 
     return app
