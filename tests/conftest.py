b29b24457783cb4e51ee1e229da8afef3849750e
Alexandre Bertuol <axdbertuol@gmail.com>
2024-06-16 19:47:09 -0300

    test: update conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,7 @@
 import pytest
 import requests
 from app import create_app, db, migrate
+from werkzeug.security import generate_password_hash
 
 
 @pytest.fixture
@@ -25,3 +26,39 @@ def app():
 @pytest.fixture
 def client(app):
     return app.test_client()
+
+
+@pytest.fixture(scope="function")
+def jwt_token(client):
+    username = "testuser"
+    password = "t3stP@ssword"
+    client.post("/auth/register", json={"username": username, "password": password})
+
+    response = client.post(
+        "/auth/login", json={"username": username, "password": password}
+    )
+    token = response.json["access_token"]
+    return token
+
+
+@pytest.fixture(scope="function")
+def create_owner(client, jwt_token):
+    owner_name = "Test Owner"
+    headers = {"Authorization": f"Bearer {jwt_token}"}
+    response = client.post("/main/owners", json={"name": owner_name}, headers=headers)
+    assert response.status_code == 201
+    owner_id = response.json["id"]
+    return owner_id
+
+
+@pytest.fixture(scope="function")
+def create_car(client, jwt_token, create_owner):
+    headers = {"Authorization": f"Bearer {jwt_token}"}
+    response = client.post(
+        "/main/cars",
+        json={"owner_id": create_owner, "color": "blue", "model": "sedan"},
+        headers=headers,
+    )
+    assert response.status_code == 201
+    car_id = response.json["id"]
+    return car_id
